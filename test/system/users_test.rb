cdb51f8ee010f9378648df6cf28b5dab69b8bf91
naofumi-fujii <naofumi.fujii@gmail.com>
2017-07-26 17:27:57 +0900

    enable test/system/users_test.rb

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,9 +1,8 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "User"
-  # end
+  test "visiting the index" do
+    visit users_url
+    assert_selector "h1", text: "User"
+  end
 end
