a9841e55b282f95f9207a54534dc5a3d5154d102
naofumi-fujii <naofumi-fujii@users.noreply.github.com>
2017-11-24 21:22:34 +0900

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../../config/environment', __FILE__)
 # require 'rails/test_help'
 require 'test/unit/rails/test_help'
+require "hogehoge"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
