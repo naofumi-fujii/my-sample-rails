914e1dd177cac30e1c2d1f7b4201ccd5016f56a0
naofumi-fujii <naofumi.fujii@gmail.com>
2017-11-24 19:36:34 +0900

    bin/rake db:migrate:reset db:seed --trace

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124102004) do
+ActiveRecord::Schema.define(version: 20171124103129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
