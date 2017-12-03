08728616dbd93d28481e6418d4c1bf4cca387d7d
naofumi-fujii <naofumi.fujii@gmail.com>
2017-12-03 17:19:50 +0900

    update config.generators

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,7 +1,7 @@
 Rails.application.config.generators do |g|
-  g.stylesheets false
-  g.javascripts false
-  g.helper false
-  g.template_engine false
-  # g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
+  g.test_framework  :test_unit, fixture: false
+  g.template_engine :erb
+  g.helper          false
+  g.stylesheets     false
+  g.javascripts     false
 end
