4b667c0ea86e5ffd7a745ce91b50bd8990ae5428
naofumi-fujii <naofumi.fujii@gmail.com>
2018-07-13 11:01:19 +0900

    $ ./bin/update

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171203075420) do
+ActiveRecord::Schema.define(version: 20180713015858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
