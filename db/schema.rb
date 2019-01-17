14aa90f04f2a87a7b469421a4575ca13a2c967aa
naofumi-fujii <naofumi.fujii@gmail.com>
2019-01-17 17:25:06 +0900

    remove admin_characters table

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_082154) do
+ActiveRecord::Schema.define(version: 2019_01_17_082114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admin_characters", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
