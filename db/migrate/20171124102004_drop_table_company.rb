3ba6c8daec0c3de61ba7369a31e1aa54b9a4f07f
naofumi-fujii <naofumi.fujii@gmail.com>
2019-04-05 10:49:51 +0900

    delete all files

--- db/migrate/20171124102004_drop_table_company.rb
+++
@@ -1,5 +0,0 @@
-class DropTableCompany < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :companies
-  end
-end
--- db/migrate/20180713015858_remove_admin_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAdminUser < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :admin_users
-  end
-end
